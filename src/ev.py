76a2aaa5df935d65c010f0ffa448774f7eff7da0
codderdreamer <sevdayucedagg@gmail.com>
2023-12-01 21:52:21 +0300

    test

--- src/ev.py
+++ src/ev.py
@@ -3,5 +3,8 @@ from src.enums import *
 class EV():
     def __init__(self):
         self.control_pilot = ControlPlot.stateF.value
+        self.current_L1 = None
+        self.current_L2 = None
+        self.current_L3 = None
 
         

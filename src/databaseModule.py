72acb3c728b44c33161a4b06e2463156b5b8d6fd
codderdreamer <sevdayucedagg@gmail.com>
2023-12-04 23:24:11 +0300

    test db

--- src/databaseModule.py
+++ src/databaseModule.py
@@ -11,10 +11,15 @@ class DatabaseModule():
         self.get_network_priority()
         self.get_settings_4g()
         self.get_wifi_settings()
+        
+        self.set_dns_settings("2.2.2.2","333")
 
     def settings_db_connect(self):
-        self.settings_database = sqlite3.connect('Settings.sqlite')
-        self.cursor = self.settings_database.cursor()
+        try:
+            self.settings_database = sqlite3.connect('Settings.sqlite')
+            self.cursor = self.settings_database.cursor()
+        except Exception as e:
+            print(e)
         
     def get_dns_settings(self):
         try:
@@ -32,69 +37,88 @@ class DatabaseModule():
         return data_dict
     
     def get_ethernet_settings(self):
-        data_dict = {}
-        query = "SELECT * FROM ethernet_settings"
-        self.cursor.execute(query)
-        data = self.cursor.fetchall()
-        for row in data:
-            data_dict[row[0]] = row[1]
-        print("get_ethernet_settings",data_dict,"\n")
-        self.application.settings.ethernetSettings.DHCPActivate = data_dict["dhcpActivate"]
-        self.application.settings.ethernetSettings.ip = data_dict["ip"]
-        self.application.settings.ethernetSettings.netmask = data_dict["netmask"]
-        self.application.settings.ethernetSettings.gateway = data_dict["gateway"]
+        try:
+            data_dict = {}
+            query = "SELECT * FROM ethernet_settings"
+            self.cursor.execute(query)
+            data = self.cursor.fetchall()
+            for row in data:
+                data_dict[row[0]] = row[1]
+            print("get_ethernet_settings",data_dict,"\n")
+            self.application.settings.ethernetSettings.DHCPActivate = data_dict["dhcpActivate"]
+            self.application.settings.ethernetSettings.ip = data_dict["ip"]
+            self.application.settings.ethernetSettings.netmask = data_dict["netmask"]
+            self.application.settings.ethernetSettings.gateway = data_dict["gateway"]
+        except Exception as e:
+            print(e)
         return data_dict
     
     def get_network_priority(self):
-        data_dict = {}
-        query = "SELECT * FROM network_priority"
-        self.cursor.execute(query)
-        data = self.cursor.fetchall()
-        for row in data:
-            data_dict[row[0]] = row[1]
-        print("get_network_priority",data_dict,"\n")
-        self.application.settings.networkPriority.first = data_dict["first"]
-        self.application.settings.networkPriority.second = data_dict["second"]
-        self.application.settings.networkPriority.third = data_dict["third"]
+        try:
+            data_dict = {}
+            query = "SELECT * FROM network_priority"
+            self.cursor.execute(query)
+            data = self.cursor.fetchall()
+            for row in data:
+                data_dict[row[0]] = row[1]
+            print("get_network_priority",data_dict,"\n")
+            self.application.settings.networkPriority.first = data_dict["first"]
+            self.application.settings.networkPriority.second = data_dict["second"]
+            self.application.settings.networkPriority.third = data_dict["third"]
+        except Exception as e:
+            print(e)
         return data_dict
     
     def get_settings_4g(self):
-        data_dict = {}
-        query = "SELECT * FROM settings_4g"
-        self.cursor.execute(query)
-        data = self.cursor.fetchall()
-        for row in data:
-            data_dict[row[0]] = row[1]
-        print("get_settings_4g",data_dict,"\n")
-        self.application.settings.settings4G.APN = data_dict["apn"]
-        self.application.settings.settings4G.user = data_dict["user"]
-        self.application.settings.settings4G.password = data_dict["password"]
-        self.application.settings.settings4G.activate = data_dict["activate"]
-        self.application.settings.settings4G.pin = data_dict["pin"]
-        self.application.settings.settings4G.encryptionType = data_dict["encryptionType"]
+        try:
+            data_dict = {}
+            query = "SELECT * FROM settings_4g"
+            self.cursor.execute(query)
+            data = self.cursor.fetchall()
+            for row in data:
+                data_dict[row[0]] = row[1]
+            print("get_settings_4g",data_dict,"\n")
+            self.application.settings.settings4G.APN = data_dict["apn"]
+            self.application.settings.settings4G.user = data_dict["user"]
+            self.application.settings.settings4G.password = data_dict["password"]
+            self.application.settings.settings4G.activate = data_dict["activate"]
+            self.application.settings.settings4G.pin = data_dict["pin"]
+            self.application.settings.settings4G.encryptionType = data_dict["encryptionType"]
+        except Exception as e:
+            print(e)
         return data_dict
     
     def get_wifi_settings(self):
-        data_dict = {}
-        query = "SELECT * FROM wifi_settings"
-        self.cursor.execute(query)
-        data = self.cursor.fetchall()
-        for row in data:
-            data_dict[row[0]] = row[1]
-        print("get_wifi_settings",data_dict,"\n")
-        self.application.settings.settings4G.wifiActivate = data_dict["wifiActivate"]
-        self.application.settings.settings4G.mod = data_dict["mod"]
-        self.application.settings.settings4G.ssid = data_dict["ssid"]
-        self.application.settings.settings4G.password = data_dict["password"]
-        self.application.settings.settings4G.encryptionType = data_dict["encryptionType"]
-        self.application.settings.settings4G.netmask = data_dict["netmask"]
-        self.application.settings.settings4G.gateway = data_dict["gateway"]
+        try:
+            data_dict = {}
+            query = "SELECT * FROM wifi_settings"
+            self.cursor.execute(query)
+            data = self.cursor.fetchall()
+            for row in data:
+                data_dict[row[0]] = row[1]
+            print("get_wifi_settings",data_dict,"\n")
+            self.application.settings.settings4G.wifiActivate = data_dict["wifiActivate"]
+            self.application.settings.settings4G.mod = data_dict["mod"]
+            self.application.settings.settings4G.ssid = data_dict["ssid"]
+            self.application.settings.settings4G.password = data_dict["password"]
+            self.application.settings.settings4G.encryptionType = data_dict["encryptionType"]
+            self.application.settings.settings4G.netmask = data_dict["netmask"]
+            self.application.settings.settings4G.gateway = data_dict["gateway"]
+        except Exception as e:
+            print(e)
         return data_dict
     
-    def set_dns_settings(self):
-        query = "UPDATE settings_4g SET key = ? WHERE value = ?"
-        self.cursor.execute(query,task)
-        self.settings_database.commit()
+    def set_dns_settings(self,dns1:str,dns2:str):
+        try:
+            query = "UPDATE dns_settings SET key = ? WHERE value = ?"
+            value = (dns1,"dns1")
+            self.cursor.execute(query,value)
+            self.settings_database.commit()
+            value = (dns2,"dns2")
+            self.cursor.execute(query,value)
+            self.settings_database.commit()
+        except Exception as e:
+            print(e)
     
     def set_ethernet_settings(self):
         pass
@@ -108,11 +132,11 @@ class DatabaseModule():
     def set_wifi_settings(self):
         pass
     
-settings_database = sqlite3.connect('Settings.sqlite')
-cursor = settings_database.cursor()
-query = "UPDATE dns_settings SET key = ? WHERE value = ?"
-task = ("deneme","dns1")
-cursor.execute(query,task)
-settings_database.commit()
+# settings_database = sqlite3.connect('Settings.sqlite')
+# cursor = settings_database.cursor()
+# query = "UPDATE dns_settings SET key = ? WHERE value = ?"
+# task = ("deneme","dns1")
+# cursor.execute(query,task)
+# settings_database.commit()
     
     

51c2984e3724b19a12f76476f83a6ed365b34fb1
codderdreamer <sevdayucedagg@gmail.com>
2023-12-10 12:07:22 +0300

    TEST 4G

--- src/networkSettings.py
+++ src/networkSettings.py
@@ -5,20 +5,42 @@ class NetworkSettings():
     def __init__(self,application) -> None:
         self.application = application
         
-    def set_4G(self,apn,user,password,pin):
+    def set_4G(self):
         connection_name = "ppp0"
-        # apn = "3gnet"
-        # user = "uninet"
-        # password = "uninet"
+        apn = "3gnet"
+        user = "uninet"
+        password = "uninet"
         print("gpio ayarlanıyor")
         os.system("gpio-test.64 w d 20 1")
         time.sleep(5)
         print("gpio ayarlandı")
-        os.system(f"nmcli connection add con-name {connection_name} ifname ttyUSB2 autoconnect yes".format(connection_name))
+        add_connection_string = "nmcli connection add con-name {0} ifname ttyUSB2 autoconnect yes".format(connection_name)
+        print(add_connection_string)
+        os.system(add_connection_string)
         print("connection ekleniyor.")
-        
-        os.system(f"type gsm apn {apn} user {user} password {password}".format(apn,user,password))
+        type_gsm_set = "type gsm apn {0} user {1} password {2}".format(apn,user,password)
+        print(type_gsm_set)
+        os.system(type_gsm_set)
         
         # os.system(f"nmcli connection up id {connection_name}")
         
-NetworkSettings(None).set_4G("3gnet","uninet","uninet","")
+NetworkSettings(None).set_4G()
+
+
+
+
+
+
+
+
+
+
+
+# ####################### 4GSET ##############################################
+# gpio-test.64 w d 20 1
+# sleep 5
+# if [ ! -f "/etc/NetworkManager/system-connections/ppp0" ];then
+#     #nmcli connection delete ppp0
+#     nmcli connection add con-name ppp0 ifname ttyUSB2 autoconnect yes \
+#     type gsm apn 3gnet user uninet password uninet
+# fi

587e8446a5bb005f6c6b74ddea3e724de00af47a
codderdreamer <sevdayucedagg@gmail.com>
2023-12-10 12:01:34 +0300

    test

--- src/networkSettings.py
+++ src/networkSettings.py
@@ -14,10 +14,10 @@ class NetworkSettings():
         os.system("gpio-test.64 w d 20 1")
         time.sleep(5)
         print("gpio ayarlandı")
-        os.system(f"nmcli connection add con-name {connection_name} ifname ttyUSB2 autoconnect yes")
+        os.system(f"nmcli connection add con-name {connection_name} ifname ttyUSB2 autoconnect yes".format(connection_name))
         print("connection ekleniyor.")
         
-        os.system(f"type gsm apn {apn} user {user} password {password}")
+        os.system(f"type gsm apn {apn} user {user} password {password}".format(apn,user,password))
         
         # os.system(f"nmcli connection up id {connection_name}")
         

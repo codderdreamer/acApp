56663ed93d2e5a1499875a51c96e106cfbdb2617
codderdreamer <sevdayucedagg@gmail.com>
2023-12-10 12:47:44 +0300

    wifi test

--- src/networkSettings.py
+++ src/networkSettings.py
@@ -17,11 +17,17 @@ class NetworkSettings():
         add_connection_string = """nmcli connection add con-name {0} ifname ttyUSB2 autoconnect yes \\type gsm apn {1} user {2} password {3}""".format(connection_name,apn,user,password)
         print(add_connection_string)
         os.system(add_connection_string)
-        
-        
         # os.system(f"nmcli connection up id {connection_name}")
         
-NetworkSettings(None).set_4G()
+    def set_wifi(self):
+        ssid = "FiberHGW_TP06BA_5GHz_EXT"
+        password = "xNUEjvX9"
+        set_wifi = 'nmcli device wifi connect "{0}" password "{1}" name wifi'.format(ssid,password)
+        os.system(set_wifi)
+        
+        
+# NetworkSettings(None).set_4G()
+NetworkSettings(None).set_wifi()
 while True:
     time.sleep(1)
 
@@ -33,6 +39,8 @@ while True:
 
 
 # ####################### 4GSET ##############################################
+# #!/bin/sh
+
 # gpio-test.64 w d 20 1
 # sleep 5
 # if [ ! -f "/etc/NetworkManager/system-connections/ppp0" ];then
@@ -40,3 +48,27 @@ while True:
 #     nmcli connection add con-name ppp0 ifname ttyUSB2 autoconnect yes \
 #     type gsm apn 3gnet user uninet password uninet
 # fi
+# ####################### 4G DEL #############################################
+#!/bin/sh
+# nmcli connection delete ppp0
+
+
+# ####################### WİFİ SET ##########################################
+# #!/bin/sh
+# stty erase ^h
+# read -p "Enter your WIFI SSID: " wifi_ssid
+# read -p "Enter your WIFI Password: " wifi_password
+# nmcli device wifi connect "$wifi_ssid" password "$wifi_password" name wifi
+
+# ####################### WİFİ MOD ##########################################
+#!/bin/sh
+# stty erase ^h
+# read -p "Enter your WIFI SSID: " wifi_ssid
+# read -p "Enter your WIFI Password: " wifi_password
+# nmcli connection modify wifi wifi.ssid "$wifi_ssid"
+# nmcli connection modify wifi wifi-sec.psk "$wifi_password"
+# nmcli connection up wifi
+
+# ####################### WİFİ DEL ##########################################
+#!/bin/sh
+# nmcli connection delete wifi

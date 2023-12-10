6f511a975623dd80aec71d6191c73d7ee240609b
codderdreamer <sevdayucedagg@gmail.com>
2023-12-10 13:36:23 +0300

    test dns

--- src/networkSettings.py
+++ src/networkSettings.py
@@ -13,6 +13,32 @@ class NetworkSettings():
         os.system(set_eth)
         os.system('nmcli con up "static-eth1" ifname eth1')
         
+    def set_dns(self):
+        dns1 = "114.114.114.114"
+        dns2 = "8.8.8.8"
+        set_dns = """# This file is managed by man:systemd-resolved(8). Do not edit.
+#
+# This is a dynamic resolv.conf file for connecting local clients to the
+# internal DNS stub resolver of systemd-resolved. This file lists all
+# configured search domains.
+#
+# Run "systemd-resolve --status" to see details about the uplink DNS servers
+# currently in use.
+#
+# Third party programs must not access this file directly, but only through the
+# symlink at /etc/resolv.conf. To manage man:resolv.conf(5) in a different way,
+# replace this symlink by a static file or a different symlink.
+#
+# See man:systemd-resolved.service(8) for details about the supported modes of
+# operation for /etc/resolv.conf.
+
+nameserver 127.0.0.53
+options edns0     
+nameserver {dns1}
+nameserver {dns2}  
+"""
+        print(set_dns)
+        
         
     def set_4G(self):
         connection_name = "ppp0"

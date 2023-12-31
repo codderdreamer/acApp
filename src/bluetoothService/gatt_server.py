8754752e739a1fbb137c8c112c166e3f7aab0eeb
codderdreamer <sevdayucedagg@gmail.com>
2023-12-31 14:16:17 +0300

    test

--- src/bluetoothService/gatt_server.py
+++ src/bluetoothService/gatt_server.py
@@ -42,7 +42,7 @@ class Application(dbus.service.Object):
         self.services = []
         dbus.service.Object.__init__(self, bus, self.path)
         self.add_service(HeartRateService(bus, 0))
-        self.add_service(BatteryService(bus, 1))
+        # self.add_service(BatteryService(bus, 1))
         self.add_service(TestService(bus, 2))
 
     def get_path(self):

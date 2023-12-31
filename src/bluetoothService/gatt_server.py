3e0b68316c3a1bb3712f02fc2d3bd00691089d97
codderdreamer <sevdayucedagg@gmail.com>
2023-12-31 20:41:34 +0300

    test ble software settings

--- src/bluetoothService/gatt_server.py
+++ src/bluetoothService/gatt_server.py
@@ -416,9 +416,9 @@ class SoftwareSettingService(Service):
     def __init__(self, bus, index, application):
         Service.__init__(self, bus, index, self.SOFTWARE_SETTİNG_UUID, True)
         self.add_characteristic(NetworkPriorityCharacteristic(bus, 0, self, application))
-        self.add_characteristic(SettingsFourGCharacteristic(bus, 0, self, application))
-        self.add_characteristic(EthernetSettingsCharacteristic(bus, 0, self, application))
-        self.add_characteristic(DNSSettingsCharacteristic(bus, 0, self, application))
+        self.add_characteristic(SettingsFourGCharacteristic(bus, 1, self, application))
+        self.add_characteristic(EthernetSettingsCharacteristic(bus, 2, self, application))
+        self.add_characteristic(DNSSettingsCharacteristic(bus, 3, self, application))
         
 class NetworkPriorityCharacteristic(Characteristic):
     

7873e144c083931548fc4a748e904b719a560fcc
codderdreamer <sevdayucedagg@gmail.com>
2024-01-07 00:17:56 +0300

    t

--- src/process.py
+++ src/process.py
@@ -22,7 +22,7 @@ class Process():
                 self.application.serialPort.set_command_pid_locker_control(LockerState.Lock)
                 time.sleep(3)
                 self.application.serialPort.get_command_pid_locker_control()
-                time.sleep(0.3)
+                time.sleep(0.5)
                 print("self.application.ev.pid_locker_control",self.application.ev.pid_locker_control)
                 if self.application.ev.pid_locker_control == LockerState.Lock.value:
                     self.application.serialPort.set_command_pid_cp_pwm(self.application.ev.proximity_pilot_current)

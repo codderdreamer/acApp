d99a994c36387c431ffef43d2746bdd6cfc92140
codderdreamer <sevdayucedagg@gmail.com>
2024-01-06 19:58:31 +0300

    t

--- src/bluetoothService/bluetoothService.py
+++ src/bluetoothService/bluetoothService.py
@@ -15,20 +15,7 @@ from src.bluetoothService import advertising
 from src.bluetoothService import gatt_server
 import time
 
-class Agent(dbus.service.Object):
-    AGENT_INTERFACE = 'org.bluez.Agent1'
 
-    def __init__(self, bus, path):
-        super().__init__(bus, path)
-
-    @dbus.service.method(AGENT_INTERFACE, in_signature="", out_signature="")
-    def Release(self):
-        print("Agent Released")
-
-    @dbus.service.method(AGENT_INTERFACE, in_signature="os", out_signature="")
-    def RequestPinCode(self, device, attempt):
-        print("*********************************RequestPinCode (%s, %d)" % (device, attempt))
-        return input("Enter PIN Code: ")  # Prompt for a PIN code
 
 class BluetoothService():
     def __init__(self,application) -> None:
@@ -40,13 +27,7 @@ class BluetoothService():
         self.mainloop = None
         Thread(target=self.run,daemon=True).start()
         
-    def setup_agent(self):
-        self.agent_path = "/test/agent"
-        self.agent = Agent(self.bus, self.agent_path)
-        agent_manager = dbus.Interface(self.bus.get_object('org.bluez', '/org/bluez'), 'org.bluez.AgentManager1')
-        agent_manager.RegisterAgent(self.agent_path, 'KeyboardDisplay')
-        agent_manager.RequestDefaultAgent(self.agent_path)
-        print("Agent registered")
+
         
     def run(self):
         self.parser = argparse.ArgumentParser()
@@ -64,7 +45,7 @@ class BluetoothService():
         
         self.mainloop.run()
         
-        self.setup_agent()
+        
         
 # bleService = BluetoothService()
 # while True:

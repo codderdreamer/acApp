21598af6a27b39c8108c5ea9288efc7b89b2364e
codderdreamer <sevdayucedagg@gmail.com>
2024-01-06 23:52:22 +0300

    test

--- src/process.py
+++ src/process.py
@@ -20,13 +20,13 @@ class Process():
         if self.application.cardType == CardType.LocalPnC:
             if self.application.socketType == SocketType.Type2:
                 self.application.serialPort.set_command_pid_locker_control(LockerState.Lock)
-                time.sleep(1)
+                time.sleep(2)
                 self.application.serialPort.get_command_pid_locker_control()
-                time.sleep(1)
+                time.sleep(2)
                 self.application.serialPort.get_command_pid_locker_control()
-                time.sleep(1)
+                time.sleep(2)
                 self.application.serialPort.get_command_pid_locker_control()
-                time.sleep(1)
+                time.sleep(2)
                 print("self.application.ev.pid_locker_control",self.application.ev.pid_locker_control)
                 if self.application.ev.pid_locker_control == LockerState.Lock.value:
                     self.application.serialPort.set_command_pid_cp_pwm(self.application.ev.proximity_pilot_current)

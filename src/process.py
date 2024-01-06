7ce4641fc9f4af51103ca43b81da0c31c5f76335
codderdreamer <sevdayucedagg@gmail.com>
2024-01-07 00:43:59 +0300

    test

--- src/process.py
+++ src/process.py
@@ -21,15 +21,17 @@ class Process():
             if self.application.socketType == SocketType.Type2:
                 self.application.serialPort.set_command_pid_locker_control(LockerState.Lock)
                 time.sleep(4)
-                self.application.serialPort.get_command_pid_locker_control()
-                time.sleep(0.3)
-                print("self.application.ev.pid_locker_control",self.application.ev.pid_locker_control)
-                if self.application.ev.pid_locker_control == LockerState.Lock.value:
-                    self.application.serialPort.set_command_pid_cp_pwm(self.application.ev.proximity_pilot_current)
-                    self.application.deviceState = DeviceState.WAITING_STATE_C
-                    print("************************* DeviceState.WAITING_STATE_C")
-                else:
-                    print("Hata Lock Connector Çalışmadı !!!")
+                time_start = time.time()
+                while time.time() - time_start < 10:
+                    self.application.serialPort.get_command_pid_locker_control()
+                    time.sleep(0.3)
+                    print("self.application.ev.pid_locker_control",self.application.ev.pid_locker_control)
+                    if self.application.ev.pid_locker_control == LockerState.Lock.value:
+                        self.application.serialPort.set_command_pid_cp_pwm(self.application.ev.proximity_pilot_current)
+                        self.application.deviceState = DeviceState.WAITING_STATE_C
+                        break
+                    else:
+                        print("Hata Lock Connector Çalışmadı !!!")
             elif self.application.socketType == SocketType.TetheredType:
                 self.application.serialPort.set_command_pid_cp_pwm(self.application.max_current)
                 self.application.deviceState = DeviceState.WAITING_STATE_C

2b86769f16e7a2fffad79fc309407c33529e6974
codderdreamer <sevdayucedagg@gmail.com>
2024-01-06 22:33:43 +0300

    test

--- src/process.py
+++ src/process.py
@@ -7,11 +7,18 @@ class Process():
         
     def connected(self):
         print("******************************************************************connected")
-        time.sleep(2)
+        time.sleep(1)
+        if self.application.deviceState != DeviceState.CONNECTED:
+            return
         if self.application.socketType == SocketType.Type2:
             self.application.serialPort.get_command_pid_proximity_pilot()
             time.sleep(0.5)
-            print("********************************************************************self.application.ev.proximity_pilot_current",self.application.ev.proximity_pilot_current)
-        
+            if self.application.ev.proximity_pilot_current == 0:
+                self.application.deviceState = DeviceState.FAULT
+                print("self.application.deviceState = DeviceState.FAULT")
+            else:
+                self.application.serialPort.set_command_pid_cp_pwm(self.application.ev.proximity_pilot_current)
+    
+    
     def run(self):
         pass

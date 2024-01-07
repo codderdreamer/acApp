7a6365a9b121bf2ca186953ef054a8dc06ca9fc8
codderdreamer <sevdayucedagg@gmail.com>
2024-01-07 16:55:25 +0300

    test ocpp

--- main.py
+++ main.py
@@ -100,28 +100,26 @@ class Application():
             
                 
         
-    # async def main(self):
-    #     try:
-    #         async with websockets.connect(self.config.ocpp_server_url + self.config.charge_point_id, subprotocols=[self.ocpp_subprotocols.value]) as ws:
-    #             if self.ocpp_subprotocols == OcppVersion.ocpp16:
-    #                 self.chargePoint = ChargePoint16(self,self.config.charge_point_id, ws)
-    #                 future = asyncio.run_coroutine_threadsafe(self.chargePoint.start(), self.loop)
-    #                 await self.chargePoint.send_boot_notification(self.config.charge_point_model,self.config.charge_point_vendor)
-    #             elif self.ocpp_subprotocols == OcppVersion.ocpp20:
-    #                 pass
-    #             elif self.ocpp_subprotocols == OcppVersion.ocpp21:
-    #                 pass
-    #     except Exception as e:
-    #         print("main",e)
+    async def ocppStart(self):
+        try:
+            async with websockets.connect(self.config.ocpp_server_url + self.config.charge_point_id, subprotocols=[self.ocpp_subprotocols.value]) as ws:
+                if self.ocpp_subprotocols == OcppVersion.ocpp16:
+                    self.chargePoint = ChargePoint16(self,self.config.charge_point_id, ws)
+                    future = asyncio.run_coroutine_threadsafe(self.chargePoint.start(), self.loop)
+                    await self.chargePoint.send_boot_notification(self.config.charge_point_model,self.config.charge_point_vendor)
+                elif self.ocpp_subprotocols == OcppVersion.ocpp20:
+                    pass
+                elif self.ocpp_subprotocols == OcppVersion.ocpp21:
+                    pass
+        except Exception as e:
+            print("main",e)
 
     
 if __name__ == "__main__":
     try:
-        # ocppActive = False
         loop = asyncio.get_event_loop()
         app = Application(loop)
-        # if ocppActive:
-        #     res = loop.run_until_complete(app.main())
+        res = loop.run_until_complete(app.ocppStart())
     except Exception as e:
         print("__main__",e)
     while True:
--- src/ensureFutures.py
+++ src/ensureFutures.py
@@ -6,23 +6,7 @@ import time
 class EnsureFutures():
     def __init__(self,application) -> None:
         self.application = application
-        self.chargePoint = None
-        self.on_run_charge_point()
-        
-    async def run_charge_point(self):
-        print("******************************************* run_charge_point")
-        try:
-            print("******************************************* websockets.connect")
-            async with websockets.connect(self.application.config.ocpp_server_url + self.application.config.charge_point_id, subprotocols=[self.application.ocpp_subprotocols.value]) as ws:
-                self.chargePoint = ChargePoint16(self,self.application.config.charge_point_id, ws)
-                future = asyncio.run_coroutine_threadsafe(self.chargePoint.start(), self.application.loop)
-                await self.chargePoint.send_boot_notification(self.application.config.charge_point_model,self.application.config.charge_point_vendor)
-        except Exception as e:
-            print("run_charge_point******************************************",e)
-        
-    def on_run_charge_point(self):
-        print("*******************************************ensure_future on_run_charge_point")
-        asyncio.ensure_future(self.run_charge_point())
+
 
     def on_authorize(self,id_tag):
         return asyncio.ensure_future(self.application.chargePoint.send_authorize(id_tag))

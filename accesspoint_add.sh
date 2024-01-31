f4753e5378374a3bf1b0daa6c059e1ad9b97a8eb
codderdreamer <sevdayucedagg@gmail.com>
2024-01-31 23:25:15 +0300

    t

--- accesspoint_add.sh
+++ accesspoint_add.sh
@@ -10,8 +10,8 @@ nmcli con modify $1 802-11-wireless-security.pairwise ccmp && \
 nmcli con modify $1 802-11-wireless-security.psk $2 
 if [ "$3" = "True" ]; then
     nmcli con modify "$1" ipv4.method manual && \
-    nmcli con modify "$1" ipv4.addresses "192.168.1.2/24" && \
-    nmcli con modify "$1" ipv4.gateway "192.168.1.1"
+    nmcli con modify "$1" ipv4.addresses "192.168.1.100/24" && \
+    nmcli con modify "$1" ipv4.gateway "192.168.1.1" 
 else
     nmcli con modify "$1" ipv4.method shared 
 fi

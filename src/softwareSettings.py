a6b9213f78bc8e231b0251996a265e0c5bba9336
codderdreamer <sevdayucedagg@gmail.com>
2024-01-31 18:56:53 +0300

    t

--- src/softwareSettings.py
+++ src/softwareSettings.py
@@ -36,7 +36,9 @@ class SoftwareSettings():
                     process = subprocess.Popen(['nmcli', 'con', 'show'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
                     stdout, stderr = process.communicate()
                     result = stdout.decode()
-                    print(result.split("\n"))
+                    for data in result.split("\n"):
+                        for data in  data.split(" "):
+                            print(data)
                     # if 'static-eth1' in stdout.decode():
                     #     subprocess.run(["nmcli", "con", "delete", "static-eth1"])
                     # if 'eth1' in stdout.decode():

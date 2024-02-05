4ed2934b0d0434dd20f9f2d671fb29cc64180579
codderdreamer <sevdayucedagg@gmail.com>
2024-02-05 16:29:13 +0300

    t

--- src/process.py
+++ src/process.py
@@ -18,7 +18,6 @@ class Process():
             time_start = time.time()
             while True:
                 self.application.serialPort.get_command_pid_locker_control()
-                time.sleep(0.3)
                 if self.application.ev.pid_locker_control == LockerState.Lock.value:
                     self.application.serialPort.set_command_pid_cp_pwm(self.application.ev.proximity_pilot_current)
                     self.application.deviceState = DeviceState.WAITING_STATE_C
@@ -29,6 +28,7 @@ class Process():
                 if time.time() - time_start > 10:
                     self.application.deviceState = DeviceState.FAULT
                     break
+                time.sleep(0.5)
         elif self.application.socketType == SocketType.TetheredType:
             self.application.serialPort.set_command_pid_cp_pwm(self.application.max_current)
             self.application.deviceState = DeviceState.WAITING_STATE_C

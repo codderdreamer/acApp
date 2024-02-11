1fb53c046f33ba01676754b27269383ade148c38
codderdreamer <sevdayucedagg@gmail.com>
2024-02-11 11:06:52 +0300

    t

--- main.py
+++ main.py
@@ -150,7 +150,7 @@ class Application():
             else:
                 ocpp_url = ws + self.settings.ocppSettings.domainName + self.settings.ocppSettings.path 
                 
-            ocpp_url = "ws://ocpp.chargehq.net/ocpp16/"
+            # ocpp_url = "ws://ocpp.chargehq.net/ocpp16/"
             print("********************************************************ocpp_url:",ocpp_url)
             async with websockets.connect(ocpp_url, subprotocols=[self.ocpp_subprotocols.value],compression=None,timeout=10) as ws:
                 self.ocppActive = True

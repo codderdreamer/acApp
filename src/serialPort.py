0a4415ba45e56c7b83c8a9d8071f96bb76f0bc5e
codderdreamer <sevdayucedagg@gmail.com>
2024-02-22 18:55:43 +0300

    t

--- src/serialPort.py
+++ src/serialPort.py
@@ -272,7 +272,7 @@ class SerialPort():
             data = self.set_command + self.pid_evse_temp + self.parameter_data + self.connector_id + "M"
             checksum = self.calculate_checksum(data)
             send_data = self.stx + data.encode('utf-8') + checksum.encode('utf-8') + self.lf
-            # print("Send get_command_pid_evse_temp -->", send_data)
+            print("Send get_command_pid_evse_temp -->", send_data)
             self.send_data_list.append(send_data)
             time.sleep(15)
         

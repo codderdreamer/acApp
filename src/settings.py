88985a1b9b425d15bb2ac191a6d0999d951b2248
codderdreamer <sevdayucedagg@gmail.com>
2024-03-03 23:39:02 +0300

    t

--- src/settings.py
+++ src/settings.py
@@ -31,7 +31,13 @@ class Settings():
     @websocketIp.setter
     def websocketIp(self, value):
         print("websocketIp",self.__websocketIp,value)
-        if self.__websocketIp != value:
+        if self.__websocketIp == None:
+            self.__websocketIp = value
+            self.set_websocket_ip(value)
+            self.application.flaskModule.host = self.__websocketIp
+            self.application.flaskModule.start()
+            print("self.application.flaskModule.start()")
+        elif self.__websocketIp != value:
             print("if self.__websocketIp != value:")
             self.application.flaskModule.stop()
             print("self.application.flaskModule.stop()")

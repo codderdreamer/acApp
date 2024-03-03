293c7d5576b4cf165c3f34c734011bb72539df2b
codderdreamer <sevdayucedagg@gmail.com>
2024-03-03 20:41:24 +0300

    t

--- src/softwareSettings.py
+++ src/softwareSettings.py
@@ -140,9 +140,9 @@ class SoftwareSettings():
                 time.sleep(5)
                 add_connection_string = """nmcli connection add con-name {0} ifname ttyUSB2 autoconnect yes \\type gsm apn {1} user {2} password {3}""".format(connection_name,apn,user,password)
                 os.system(add_connection_string)
-                if pin:
-                    set_pin_command = """nmcli modem gsm unlock sim-pin {0}""".format(pin)
-                    os.system(set_pin_command)
+                # if pin:
+                #     set_pin_command = """nmcli modem gsm unlock sim-pin {0}""".format(pin)
+                #     os.system(set_pin_command)
                 os.system("""nmcli con up "{0}" ifname ttyUSB2""".format(connection_name))
             else:
                 pass

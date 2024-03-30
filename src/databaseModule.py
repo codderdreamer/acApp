e9c25b82dc6475fb49deddc458edaa6db2592e63
codderdreamer <sevdayucedagg@gmail.com>
2024-03-30 18:45:18 +0300

    t

--- src/databaseModule.py
+++ src/databaseModule.py
@@ -649,9 +649,9 @@ class DatabaseModule():
         try:
             self.settings_database = sqlite3.connect('/root/acApp/Settings.sqlite')
             self.cursor = self.settings_database.cursor()
-            query = "UPDATE user_login SET Password = ? WHERE UserName = 'HCAC'"
+            query = "UPDATE device_settings SET Password = ? WHERE UserName = ?"
             
-            value = (password)
+            value = (password,"HCAC")
             self.cursor.execute(query,value)
             self.settings_database.commit()
             

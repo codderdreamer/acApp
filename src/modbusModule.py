5cec3ca9469a42560ba799bec52a89a94b22ed41
codderdreamer <sevdayucedagg@gmail.com>
2024-04-01 00:04:38 +0300

    t

--- src/modbusModule.py
+++ src/modbusModule.py
@@ -65,6 +65,12 @@ class ModbusModule:
                 self.total_energy_import = round(self.read_input_float(register_address=73)/1000,2)
                 self.total_energy_export = round(self.read_input_float(register_address=75)/1000,2)
                 self.current_demand = self.read_input_float(register_address=259)
+                print("self.volt_l1",self.volt_l1)
+                print("self.volt_l2",self.volt_l2)
+                print("self.volt_l3",self.volt_l3)
+                print("self.current_l1",self.volt_l3)
+                print("self.current_l2",self.volt_l3)
+                print("self.current_l3",self.volt_l3)
                 self.connection = True
             except Exception as e:
                 # print(e)

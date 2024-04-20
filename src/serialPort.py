a9bc655369095a99d870078fdcfdc4e97a5a7491
codderdreamer <sevdayucedagg@gmail.com>
2024-04-20 17:04:55 +0300

    t

--- src/serialPort.py
+++ src/serialPort.py
@@ -321,7 +321,7 @@ class SerialPort():
         checksum = self.calculate_checksum(data)
         send_data = self.stx + data.encode('utf-8') + checksum.encode('utf-8') + self.lf
         self.send_data_list.append(send_data)
-        time.sleep(2)
+        time.sleep(4)
         print("Başlangıçta error durumu kontrol ediliyor..")
         print("pid_error_list",self.error_list)
         if len(self.error_list)>0:

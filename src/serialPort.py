8131a6c10809ce0f811b20c1aaa320541327d559
codderdreamer <sevdayucedagg@gmail.com>
2024-04-27 15:10:49 +0300

    t

--- src/serialPort.py
+++ src/serialPort.py
@@ -206,10 +206,10 @@ class SerialPort():
         data = self.set_command + self.pid_led_control + self.parameter_data + self.connector_id + led_state.value
         checksum = self.calculate_checksum(data)
         send_data = self.stx + data.encode('utf-8') + checksum.encode('utf-8') + self.lf
-        print("Send set_command_pid_led_control -->", led_state)
+        print("************************* led_state -->", led_state)
         self.send_data_list.append(send_data)
         if led_state != LedState.RfidVerified and led_state != LedState.RfidFailed:
-            print("********** önceki led setlendi",self.led_state)
+            # print("********** önceki led setlendi",self.led_state)
             self.led_state = led_state
         else:
             time.sleep(2)

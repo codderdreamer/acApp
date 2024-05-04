69b3a2fa15746e0715f5517151785e094f4674b2
codderdreamer <sevdayucedagg@gmail.com>
2024-05-04 12:00:34 +0300

    t

--- src/softwareSettings.py
+++ src/softwareSettings.py
@@ -174,8 +174,7 @@ class SoftwareSettings():
                 os.system(add_connection_string)
                 # time.sleep(1)
                 if pin:
-                    set_pin_command = """nmcli connection modify {0} gsm.pin {1}""".format(connection_name,pin)
-                    os.system(set_pin_command)
+                    os.system("""mmcli -i 0 --pin={0}""".format(connection_name))
                 os.system("""nmcli con up "{0}" ifname ttyUSB2""".format(connection_name))
             else:
                 pass

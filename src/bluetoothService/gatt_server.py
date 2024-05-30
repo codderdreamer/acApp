92de1875394d8cc4b04dd2dfc00a213ba6f0ddca
codderdreamer <sevdayucedagg@gmail.com>
2024-05-30 19:43:21 +0300

    t

--- src/bluetoothService/gatt_server.py
+++ src/bluetoothService/gatt_server.py
@@ -35,7 +35,6 @@ class ApplicationBluetooth(dbus.service.Object):
         self.path = '/'
         self.services = []
         dbus.service.Object.__init__(self, bus, self.path)
-        # self.add_service(HeartRateService(bus, 0))
         self.add_service(SoftwareSettingsService(bus, 0, self.application))
 
     def get_path(self):
@@ -270,108 +269,6 @@ class Descriptor(dbus.service.Object):
         print('Default WriteValue called, returning error')
         raise exceptions.NotSupportedException()
 
-class HeartRateService(Service):
-    """
-    Fake Heart Rate Service that simulates a fake heart beat and control point
-    behavior.
-    """
-    HR_UUID = '0000180d-0000-1000-8000-00805f9b34fb'
-
-    def __init__(self, bus, index):
-        Service.__init__(self, bus, index, self.HR_UUID, True)
-        self.add_characteristic(HeartRateMeasurementChrc(bus, 0, self))
-        self.add_characteristic(BodySensorLocationChrc(bus, 1, self))
-        self.add_characteristic(HeartRateControlPointChrc(bus, 2, self))
-        self.energy_expended = 0
-
-class HeartRateMeasurementChrc(Characteristic):
-    HR_MSRMT_UUID = '00002a37-0000-1000-8000-00805f9b34fb'
-
-    def __init__(self, bus, index, service):
-        Characteristic.__init__(
-            self, bus, index,
-            self.HR_MSRMT_UUID,
-            ['notify', 'secure-read', 'secure-write'],
-            service)
-        self.notifying = False
-        self.hr_ee_count = 0
-
-    def hr_msrmt_cb(self):
-        try:
-            value = []
-            value.append(dbus.Byte(0x06))
-            value.append(dbus.Byte(randint(90, 130)))
-            if self.hr_ee_count % 10 == 0:
-                value[0] = dbus.Byte(value[0] | 0x08)
-                value.append(dbus.Byte(self.service.energy_expended & 0xff))
-                value.append(dbus.Byte((self.service.energy_expended >> 8) & 0xff))
-            self.service.energy_expended = \
-                min(0xffff, self.service.energy_expended + 1)
-            self.hr_ee_count += 1
-            self.PropertiesChanged(GATT_CHRC_IFACE, {'Value': value}, [])
-            return self.notifying
-        except Exception as e:
-            print(datetime.now(), "hr_msrmt_cb Exception:", e)
-
-    def _update_hr_msrmt_simulation(self):
-        try:
-            if not self.notifying:
-                return
-            GObject.timeout_add(1000, self.hr_msrmt_cb)
-        except Exception as e:
-            print(datetime.now(), "_update_hr_msrmt_simulation Exception:", e)
-
-    def StartNotify(self):
-        try:
-            if self.notifying:
-                return
-            self.notifying = True
-            self._update_hr_msrmt_simulation()
-        except Exception as e:
-            print(datetime.now(), "StartNotify Exception:", e)
-
-    def StopNotify(self):
-        try:
-            if not self.notifying:
-                return
-            self.notifying = False
-            self._update_hr_msrmt_simulation()
-        except Exception as e:
-            print(datetime.now(), "StopNotify Exception:", e)
-
-class BodySensorLocationChrc(Characteristic):
-    BODY_SNSR_LOC_UUID = '00002a38-0000-1000-8000-00805f9b34fb'
-
-    def __init__(self, bus, index, service):
-        Characteristic.__init__(
-            self, bus, index,
-            self.BODY_SNSR_LOC_UUID,
-            ['read', 'secure-read'],
-            service)
-
-    def ReadValue(self, options):
-        return [0x01]  # Return 'Chest' as the sensor location.
-
-class HeartRateControlPointChrc(Characteristic):
-    HR_CTRL_PT_UUID = '00002a39-0000-1000-8000-00805f9b34fb'
-
-    def __init__(self, bus, index, service):
-        Characteristic.__init__(
-            self, bus, index,
-            self.HR_CTRL_PT_UUID,
-            ['write', 'secure-write'],
-            service)
-
-    def WriteValue(self, value, options):
-        try:
-            if len(value) != 1:
-                raise exceptions.InvalidValueLengthException()
-            byte = value[0]
-            if byte != 1:
-                raise exceptions.FailedException("0x80")
-            self.service.energy_expended = 0
-        except Exception as e:
-            print(datetime.now(), "WriteValue Exception:", e)
 
 class SoftwareSettingsService(Service):
     SoftwareSettings_UUID = '12345678-1234-5678-1234-56789abcab00'

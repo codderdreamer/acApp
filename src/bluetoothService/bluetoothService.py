e7d730c88c9933e2f9aa9f2f24181f49ca1bfc1b
codderdreamer <sevdayucedagg@gmail.com>
2024-05-30 21:00:12 +0300

    t

--- src/bluetoothService/bluetoothService.py
+++ src/bluetoothService/bluetoothService.py
@@ -49,7 +49,7 @@ class BluetoothService():
             agent = Agent(self.bus, path)
             obj = self.bus.get_object("org.bluez", "/org/bluez")
             manager = dbus.Interface(obj, "org.bluez.AgentManager1")
-            manager.RegisterAgent(path, "KeyboardDisplay")
+            manager.RegisterAgent(path, "NoInputNoOutput")
             manager.RequestDefaultAgent(path)
             print("Agent registered successfully")
         except Exception as e:

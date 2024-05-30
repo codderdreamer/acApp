2e394d37eb9dc9092730e7a88c1a094735ddde7f
codderdreamer <sevdayucedagg@gmail.com>
2024-05-30 20:18:10 +0300

    t

--- src/bluetoothService/bluetoothService.py
+++ src/bluetoothService/bluetoothService.py
@@ -37,8 +37,8 @@ class BluetoothService():
             self.bus = dbus.SystemBus()
             self.mainloop = GObject.MainLoop()
             self.register_agent()
-            advertising.advertising_main(self.mainloop, self.bus, self.adapter_name)
-            gatt_server.gatt_server_main(self.application, self.mainloop, self.bus, self.adapter_name)
+            # advertising.advertising_main(self.mainloop, self.bus, self.adapter_name)
+            # gatt_server.gatt_server_main(self.application, self.mainloop, self.bus, self.adapter_name)
             self.mainloop.run()
         except Exception as e:
             print(datetime.now(), "BluetoothService run Exception:", e)

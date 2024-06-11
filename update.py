7bdc2c0e57bda4cbdb51035f730faeb147e5bb7c
codderdreamer <sevdayucedagg@gmail.com>
2024-06-11 10:55:00 +0300

    t

--- update.py
+++ update.py
@@ -6,28 +6,22 @@ import subprocess
 
 def check_for_git_changes():
     try:
-        # Doğru dizine gitmek için os.chdir kullan
         os.chdir("/root/acApp")
-        # Git status komutunu çalıştır
-                # Git fetch komutunu çalıştır
         fetch_result = subprocess.run(['git', 'fetch'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
-        
-        # Yeni commit'leri kontrol etmek için git log komutunu çalıştır
         log_result = subprocess.run(['git', 'log', 'HEAD..origin/main', '--oneline'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
         log_output = log_result.stdout
         log_error = log_result.stderr
-        
         if log_result.returncode != 0:
             print("Log error:", log_error)
-            return
-        
         if log_output:
             print("New commits:\n", log_output)
+            return True
         else:
             print("No new commits.")
+            return False
     except Exception as e:
         print("check_for_git_changes An error occurred:", e)
-        # return True
+        # burada hataya düşerse ne olcak?
 
 def is_there_charge():
     file_path = "/root/Charge.sqlite"
@@ -41,12 +35,10 @@ def is_there_charge():
                 query = "SELECT * FROM ev"
                 cursor.execute(query)
                 data = cursor.fetchall()
+                charge_database.close()
                 for row in data:
                     data_dict[row[0]] = row[1]
-                print(data_dict["charge"]=="True")
-                
-                print("Charge:", data_dict.get("charge", "False"))
-                return data_dict.get("charge", "False") == "True"
+                return data_dict["charge"]=="True"
     except sqlite3.Error as e:
         print("SQLite error:", e)
         return False
@@ -54,18 +46,25 @@ def is_there_charge():
         print("Dosya kilitli")
         time.sleep(1)
         return is_there_charge()
+    except Exception as e:
+        print("is_there_charge Exception:", e)
+        return is_there_charge()
+    
+def updade_firmware():
+    os.system("pwd")
+    print("pwd")
 
+charge = False
+there_is_change = False
 while True:
     try:
-        # Devam eden bir şarj var mı?
-        if is_there_charge():
-            pass # işlem yapma bekle
-        else:
-            # Git üzerinde değişiklik var mı kontrol et
-            check_for_git_changes()
-            #     print("Git üzerinde değişiklik var! Update yapılacak")
-            # else:
-            #     print("Git üzerinde bir değişiklik yok...")
+        
+        charge = is_there_charge()
+        if charge == False:
+            there_is_change = check_for_git_changes()
+        if there_is_change == True:
+            updade_firmware()
+
     except Exception as e:
         print("Exception:", e)
         

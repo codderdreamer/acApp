c264e564422ce7ff1bad4d62376e4d7f63d71eb4
codderdreamer <sevdayucedagg@gmail.com>
2024-07-12 10:31:43 +0300

    t

--- main.py
+++ main.py
@@ -180,7 +180,7 @@ class Application():
                     self.ev.voltage_L1 = self.modbusModule.voltage_L1
                     self.ev.voltage_L2 = self.modbusModule.voltage_L2
                     self.ev.voltage_L3 = self.modbusModule.voltage_L3
-                    self.ev.energy = self.modbusModule.energy - self.modbusModule.firstEnergy
+                    self.ev.energy = round(self.modbusModule.energy - self.modbusModule.firstEnergy,2)
                     self.ev.power =  self.modbusModule.power
                 elif (self.settings.deviceSettings.mid_meter == False and self.settings.deviceSettings.externalMidMeter == False):
                     # print("Veriler MCU'dan alınıyor...")
@@ -190,7 +190,7 @@ class Application():
                     self.ev.voltage_L1 = self.serialPort.voltage_L1
                     self.ev.voltage_L2 = self.serialPort.voltage_L2
                     self.ev.voltage_L3 = self.serialPort.voltage_L3
-                    self.ev.energy = self.serialPort.energy
+                    self.ev.energy = round(self.serialPort.energy,2)
                     self.ev.power =  self.serialPort.power
                 else:
                     self.ev.current_L1 = 0

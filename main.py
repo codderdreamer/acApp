546b5852d088e0410b7680da15396d749567dfce
codderdreamer <sevdayucedagg@gmail.com>
2024-08-12 13:23:53 +0300

    t

--- main.py
+++ main.py
@@ -25,11 +25,15 @@ from src.webSocket import *
 import builtins
 from src.logger import ac_app_logger as logger
 
+
+file = open("/root/output.txt", "a")
+
 original_print = builtins.print
 def timestamped_print(color = "",*args, **kwargs):
     current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     args = (f"[{current_time}]",) + (color,) + args + ("\033[0m",)
     original_print(*args, **kwargs)
+    file.write(" ".join(map(str, args)) + "\n")
 builtins.print = timestamped_print
 
 class Application():

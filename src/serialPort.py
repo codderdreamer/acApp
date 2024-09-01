fa0e89431ef495073578bb1e5303f0d8cc478a9b
codderdreamer <sevdayucedagg@gmail.com>
2024-09-01 20:39:37 +0300

    t

--- src/serialPort.py
+++ src/serialPort.py
@@ -478,7 +478,7 @@ class SerialPort():
             if card_id_length > 0:
                 for i in range(9,9+card_id_length):
                     card_id += data[i]
-            # print("card_id",card_id)
+            print("Readed card id",card_id)
             self.set_command_pid_rfid()
             if card_id != "":
                 if time.time() - self.time_rfid > 10:

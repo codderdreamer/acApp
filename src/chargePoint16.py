dafdeaa3d460781d1a57131759fd3e816885cc66
codderdreamer <sevdayucedagg@gmail.com>
2024-09-06 14:25:34 +0300

    t

--- src/chargePoint16.py
+++ src/chargePoint16.py
@@ -232,9 +232,9 @@ class ChargePoint16(cp):
 
     def send_stop_thread(self):
         try:
-            if self.application.databaseModule.get_charge()["charge"] == "True" and self.application.cardType == CardType.BillingCard:
-                self.application.process.transaction_id = self.application.databaseModule.get_charge()["transaction_id"]
-                self.application.process.id_tag = self.application.databaseModule.get_charge()["id_tag"]
+            if self.application.process.initially_charge and self.application.cardType == CardType.BillingCard:
+                print("Daha önce başlamış bir şarj vardı. Durduruluyor...")
+                print("self.application.process.transaction_id",self.application.process.transaction_id,"self.application.process.id_tag",self.application.process.id_tag)
                 asyncio.run_coroutine_threadsafe(self.application.chargePoint.send_stop_transaction(Reason.power_loss),self.application.loop)
                 time.sleep(1)
                 self.application.process.transaction_id = None

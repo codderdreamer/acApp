5edc9f9ad5a4affea8966da220700bb69207f9a2
codderdreamer <sevdayucedagg@gmail.com>
2024-09-06 15:37:41 +0300

    t

--- src/chargePoint16.py
+++ src/chargePoint16.py
@@ -239,6 +239,7 @@ class ChargePoint16(cp):
                 time.sleep(1)
                 self.application.process.transaction_id = None
                 self.application.process.id_tag = None
+                self.application.process.initially_charge = False
         except Exception as e:
             print("send_stop_thread Exception:",e)
 

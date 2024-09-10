43f6e0d84b8245f04a9d1faf11c9a5d089898731
codderdreamer <sevdayucedagg@gmail.com>
2024-09-10 15:14:09 +0300

    t

--- src/serialPort.py
+++ src/serialPort.py
@@ -94,7 +94,7 @@ class SerialPort():
                     self.send_data_list.pop(0)
             except Exception as e:
                 print("write Exception:",e)
-            time.sleep(0.05)
+            time.sleep(0.1)
 
     def calculate_checksum(self,data):
         checksum =  int.from_bytes(self.stx, "big")
@@ -505,4 +505,4 @@ class SerialPort():
                         self.set_response_pid_rfid(incoming)
             except Exception as e:
                 print("read Exception",e)
-            time.sleep(0.01)
+            time.sleep(0.1)
